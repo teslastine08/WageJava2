8b2030f87b658bf18665ef6a1775683234051611
teslastine08 <teslastine08@gmail.com>
2020-06-23 23:42:21 +0530

    U8: Company name added

--- wage.java
+++ wage.java
@@ -1,15 +1,23 @@
-import java.util.Scanner;
 public class wage
 
 {
         int workinghrs = 8;
         int dailywage = 20;
-
+        static int salary;
+        static{
+                salary=10000;
+                System.out.println("Total Salary: "+salary);
+                }
         public int calculatesalary(int workinghrs, int dailywage)
                 {
                 int salary = workinghrs*dailywage;
                 return salary;
                 }
+
+	public static String getcompanyname()
+		{
+		return "HNG Tech";
+		}
 
         public static void main(String args[])
         {
@@ -18,10 +26,10 @@ public class wage
 
 
                 wage empwage = new wage();
+		
+		System.out.println("Company Name: "+getcompanyname());
+                System.out.println("Employee salary: Rs."+empwage.calculatesalary(empwage.workinghrs, empwage.dailywage));
 
-                System.out.println("Employee salary: Rs."+empwage.calculatesalary(empwage.workinghrs, empwage.dailywage) );
         }
 
 }
-
-
