8feb7eb9fe251b59e9df7103c58593b143c0851f
teslastine08 <teslastine08@gmail.com>
2020-06-23 20:17:53 +0530

    UC1: Attandance

--- wage.java
+++ wage.java
@@ -7,6 +7,17 @@ public class wage
 
 		System.out.println("Welcome to wage Computation");
 
+		int is_part = 1;
+		int is_full = 2;
+		int emp_rate_per_hour = 20;
+
+
+		int emp_hrs = 0;
+		int emp_wage = 0;
+
+		double attandance = Math.floor(Math.random()*10)%3;
+
+		System.out.println("Attandance is: "+attandance);
 	}
 
 }
