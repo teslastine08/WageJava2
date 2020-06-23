26a947d8e04fc4da8b48b1c612aa6a7b7e2200cf
teslastine08 <teslastine08@gmail.com>
2020-06-23 21:13:02 +0530

    UC4: Switch case

--- wage.java
+++ wage.java
@@ -1,30 +1,34 @@
 public class wage
 
 {
+	public static final int is_part = 1;
+	public static final int is_full = 2;
+	public static final int emp_rate_per_hour = 20;
 
 	public static void main(String args[]) 
 	{
 
 		System.out.println("Welcome to wage Computation");
 
-		int is_part = 1;
-		int is_full = 2;
-		int emp_rate_per_hour = 20;
-
 
 		int emp_hrs = 0;
 		int emp_wage = 0;
 
-		double attandance = Math.floor(Math.random()*10)%3;
+		int attandance =  (int) Math.floor(Math.random()*10)%3;
 		System.out.println("Attandance is: "+attandance);
 
 		
-		if( attandance == is_full )
-			emp_hrs = 8;
-		else if( attandance == is_part )
-			emp_hrs = 4;
-		else
-			emp_hrs = 0;
+		switch (attandance)
+		{
+			case is_part:
+				emp_hrs = 4;
+				break;
+			case is_full:
+				emp_hrs = 8;
+				break;
+			default:
+				emp_hrs = 0;
+		}
 
 		emp_wage = emp_hrs * emp_rate_per_hour;
 		System.out.println("Daily Wage: Rs."+emp_wage);
