454e072bc75cba82f2288998cc3587cafb851099
teslastine08 <teslastine08@gmail.com>
2020-06-23 21:57:31 +0530

    UC6: Wage till 100 working hours

--- wage.java
+++ wage.java
@@ -5,6 +5,7 @@ public class wage
 	public static final int is_full = 2;
 	public static final int emp_rate_per_hour = 20;
 	public static final int no_of_working_days = 20;
+	public static final int max_hours_mnths = 100;
 
 	public static void main(String args[]) 
 	{
@@ -13,12 +14,13 @@ public class wage
 
 
 		int emp_hrs = 0;
-		int emp_wage = 0;
-		int total_emp_wage = 0;
-
-		for(int day = 0; day < no_of_working_days; day++)
-
+		int total_emp_hrs = 0;
+		int total_work_days = 0;
+		
+		while ( total_emp_hrs <= max_hours_mnths && total_work_days < no_of_working_days)
 		{
+		total_work_days++;
+
 		int attandance =  (int) Math.floor(Math.random()*10)%3;
 		System.out.println("Attandance is: "+attandance);
 
@@ -35,12 +37,11 @@ public class wage
 				emp_hrs = 0;
 		}
 
-		emp_wage = emp_hrs * emp_rate_per_hour;
-		System.out.println("Daily Wage: Rs."+emp_wage);
-
-		total_emp_wage += emp_wage;
+		total_emp_hrs += emp_hrs;
+		System.out.println("Day: "+total_work_days + " emp hrs: "+emp_hrs);
 
 		}
+		int total_emp_wage = total_emp_hrs * emp_rate_per_hour;
 		System.out.println("Total employee wage: Rs."+total_emp_wage);
 	}
 
