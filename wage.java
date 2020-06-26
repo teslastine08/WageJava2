e9e9d186018f4cf9669b62d63fb50b2dcab7f03e
Ankan Sinha <ankansinha1@gmail.com>
2020-06-26 18:57:06 +0530

    Update wage.java

--- wage.java
+++ wage.java
@@ -1,40 +1,4 @@
 public class wage
-
-{
-        int workinghrs = 8;
-        int dailywage = 20;
-        static int salary;
-        static{
-                salary=10000;
-                System.out.println("Total Salary: "+salary);
-                }
-        public int calculatesalary(int workinghrs, int dailywage)
-                {
-                int salary = workinghrs*dailywage;
-                return salary;
-                }
-
-	public static String getcompanyname()
-		{
-		return "HNG Tech";
-		}
-
-        public static void main(String args[])
-        {
-
-                System.out.println("Welcome to wage Computation");
-
-
-                wage empwage = new wage();
-		
-		System.out.println("Company Name: "+getcompanyname());
-                System.out.println("Employee salary: Rs."+empwage.calculatesalary(empwage.workinghrs, empwage.dailywage));
-
-        }
-
-}
-
-
 {
         public static final int Is_Parttime=1;
         public static final int Is_Fulltime=2;
@@ -86,4 +50,5 @@ public static void main(String[] args) {
         ITC.wage_compute();
         System.out.println(ITC);
 
+}
 }
