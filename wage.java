8d742a3c1a19025f61070cfde47c4e5a5be25f13
Ankan Sinha <ankansinha1@gmail.com>
2020-06-26 16:33:23 +0530

    Update wage.java

--- wage.java
+++ wage.java
@@ -1,35 +1,52 @@
 public class wage
 
 {
-        int workinghrs = 8;
-        int dailywage = 20;
-        static int salary;
-        static{
-                salary=10000;
-                System.out.println("Total Salary: "+salary);
-                }
-        public int calculatesalary(int workinghrs, int dailywage)
-                {
-                int salary = workinghrs*dailywage;
-                return salary;
-                }
+	public static final int is_part = 1;
+	public static final int is_full = 2;
 
-	public static String getcompanyname()
+	public static int calculate_wage(String Company, int emp_rate_per_hour, int no_of_working_days, int max_hours_mnths) 
+	{
+
+		System.out.println("Welcome to wage Computation");
+
+
+		int emp_hrs = 0;
+		int total_emp_hrs = 0;
+		int total_work_days = 0;
+		
+		while ( total_emp_hrs <= max_hours_mnths && total_work_days < no_of_working_days)
 		{
-		return "HNG Tech";
+		total_work_days++;
+
+		int attandance =  (int) Math.floor(Math.random()*10)%3;
+		System.out.println("Attandance is: "+attandance);
+
+		
+		switch (attandance)
+		{
+			case is_part:
+				emp_hrs = 4;
+				break;
+			case is_full:
+				emp_hrs = 8;
+				break;
+			default:
+				emp_hrs = 0;
 		}
 
-        public static void main(String args[])
+		total_emp_hrs += emp_hrs;
+		System.out.println("Day: "+total_work_days + " emp hrs: "+emp_hrs);
+
+		}
+		int total_emp_wage = total_emp_hrs * emp_rate_per_hour;
+		System.out.println("Total employee wage: Rs."+total_emp_wage);
+                return total_emp_wage();
+	}
+        
+        publi stati void main(String[] args)
         {
-
-                System.out.println("Welcome to wage Computation");
-
-
-                wage empwage = new wage();
-		
-		System.out.println("Company Name: "+getcompanyname());
-                System.out.println("Employee salary: Rs."+empwage.calculatesalary(empwage.workinghrs, empwage.dailywage));
-
+                calculate_wage("HNG Tech", 20, 30 , 100 );
+		calculate_wage("ITC", 30, 22 , 80 );
         }
 
 }
